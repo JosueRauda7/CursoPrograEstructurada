dfa19d8eeedf8685bc48f09830d1ba8901df6c2c
JosueRauda7 <katerozarack@gmail.com>
2020-01-27 20:13:02 -0600

    Reto 1. Completado

--- MiPrimeraVariable/main.c
+++ MiPrimeraVariable/main.c
@@ -4,12 +4,36 @@
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
-    bool boolean = false;
+    //Declaración de variables
+    int integerA;
+    float floatA;
+    double doubleA;
+    char letterA;
+    bool boolean;
 
+    //INPUTS
+    //Pidiendo valor de entero A
+    printf("Ingresa el valor de un entero A: ");
+    //Recibiendo valor de entero A
+    scanf("%i", &integerA);
+
+    //Pidiendo valor de float A
+    printf("Ingresa el valor de un float A: ");
+    //Recibiendo valor de float A
+    scanf("%f", &floatA);
+
+    //Pidiendo valor de double A
+    printf("Ingresa el valor de un double A: ");
+    //Recibiendo valor de double A
+    scanf("%f", &doubleA);
+
+    //Pidiendo valor de letter A
+    printf("Ingresa el valor de un letter A: ");
+    //Recibiendo valor de letter A
+    scanf(" %c", &letterA);
+
+    //OUTPUTS
+    printf("\n\n");
     //En la siguiente línea estamos imprimiendo mi variable integerA
     printf("El valor de nuestro entero A es: %i", integerA);
     //Vamos a imprimir un float
@@ -23,7 +47,7 @@ int main()
     printf("\nEl valor de mi caracter A es: %c", letterA);
 
     //Todo en una sola línea
-    printf("\n\tEl valor de nuestro entero A es: %i, \n\tEl valor de mi flotante A es: %f,\n\tEl valor de mi double A es: %f,\nEl valor de mi caracter A es: %c", integerA, floatA, doubleA, letterA);
+    //printf("\n\tEl valor de nuestro entero A es: %i, \n\tEl valor de mi flotante A es: %f,\n\tEl valor de mi double A es: %f,\nEl valor de mi caracter A es: %c", integerA, floatA, doubleA, letterA);
 
     return 0;
 }
